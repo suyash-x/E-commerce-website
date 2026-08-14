31d18e5f9d6bc7f41c84c9f7a2e4d4862b6680e1
suyash-x <suyashbajpaino.1@gmail.com>
2026-08-14 17:57:55 +0530

    Configure STATIC_ROOT for production

--- MyProject/MyProject/settings.py
+++ MyProject/MyProject/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_DIR=BASE_DIR/'static'
 STATICFILES_DIRS=[STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
